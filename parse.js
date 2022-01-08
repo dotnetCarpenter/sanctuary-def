37c743d915e197997dc0761c782075d0f6f3e581
dotnetCarpenter <jon.ronnenberg@gmail.com>
2022-01-08 20:05:38 +0100

    do some analysis I guess

---
+++ parse.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const types = require ('./typesObjects');
+const debug = console.debug;
+
+// types.map (x => x)
+
+// typeObject :: Array (Value Type ValidationResponse )
+
+debug (types.length)
